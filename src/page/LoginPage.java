f9921e8640b1c7b9616815232d48d5dd3bdd5637
noort <noort@NoorTeck.mynetworksettings.com>
2023-04-03 11:05:21 -0400

    Initial Commit

---
+++ src/page/LoginPage.java
@@ -0,0 +1,8 @@
+package page;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		System.out.println("Login Page Class");
+	}
+}
---
+++ src/page/RegisterPage.java
@@ -0,0 +1,7 @@
+package page;
+
+public class RegisterPage {
+	public static void main(String[] args) {
+		System.out.println("Register Page Class");
+	}
+}
