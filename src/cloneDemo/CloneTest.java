b69c4ca362242457dc728063d35c9f2b3d7486c2
Mohamed <Mohamed@DESKTOP-KR6K8PM.mynetworksettings.com>
2023-04-11 17:29:47 -0400

    commit

---
+++ src/cloneDemo/CloneTest.java
@@ -0,0 +1,18 @@
+package cloneDemo;
+
+public class CloneTest {
+	public static void main(String[] args) {
+		System.out.println("Clone Test....");
+	}
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int sub(int a, int b) {
+		return a - b;
+	}
+	
+	int age =30;
+
+}
