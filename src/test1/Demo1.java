27786f4acb1a5e391f6dc200a5bedb52d78b19f2
Running Bird <Running Bird@DESKTOP-GE19MGH>
2023-04-12 19:56:39 -0400

    value changes

--- src/test1/Demo1.java
+++ src/test1/Demo1.java
@@ -3,9 +3,9 @@ package test1;
 public class Demo1 {
 
 	public void subtract() {
-		System.out.println(3-1);
+		System.out.println(4-1);
 	}
 	public void add() {
-		System.out.println(2+2);
+		System.out.println(2+1);
 	}
 }
