635ba98d97443c4ebf2e2885a3f6563bf755e8f2
noort <noort@NoorTeck.mynetworksettings.com>
2023-04-16 20:36:08 -0400

    implemented add()

--- src/page/Day4GitDemo.java
+++ src/page/Day4GitDemo.java
@@ -6,6 +6,11 @@ public class Day4GitDemo {
 		System.out.println("THis is random line of code....");
 	}
 	
+	
+	public static void add(int a, int b) {
+		System.out.println(a+b);
+	}
+	
 	// this is comment...
 
 }
