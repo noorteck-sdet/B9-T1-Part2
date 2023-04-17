e638fe60a5e8c26eca4936ed648f21aca653413b
Running Bird <Running Bird@DESKTOP-GE19MGH>
2023-04-17 18:20:49 -0400

    pushing without saving/stashing/or pulling

--- src/test2/Test3.java
+++ src/test2/Test3.java
@@ -5,6 +5,7 @@ public class Test3 {
 	public void printLN() {
 		System.out.println("Yaaay");
 		System.out.println("hello");
+		System.out.println("eating");
 	}
 	
 	public static double finalPrice(double total) {
