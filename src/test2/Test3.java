bb734f633874dfd57e94af004d9286ac37f6b226
Running Bird <Running Bird@DESKTOP-GE19MGH>
2023-04-17 21:20:39 -0400

    hgfcd

--- src/test2/Test3.java
+++ src/test2/Test3.java
@@ -6,6 +6,9 @@ public class Test3 {
 		System.out.println("Yaaay");
 		System.out.println("hello");
 		System.out.println("eating");
+		System.out.println("trying to push without stashingo or pulling, then realizing i hadn't pulled,"
+				+ "pullingt, and then resolving the error");
+		
 	}
 	
 	public static double finalPrice(double total) {
